5d1dd012a4e0f4592c6ceef81d7e17f39d423f4c
philosofool <lenhart.lenhart@gmail.com>
2026-02-01 19:51:10 +0000

    Add create_jazz_table.py

--- queries/recording_to_album.sql
+++ queries/recording_to_album.sql
@@ -10,25 +10,35 @@
 
 	What's this for? The main role here is to filter
 */
-WITH recording_to_album AS (
-
+--WITH recording_to_album AS (
 	SELECT
-		MIN(recording.name) as title,
-		MIN(release_group.name) as album,
+		recording.name as title,
+		release_group.name as album,
+		artist_credit.name as artist,
 		recording.id as performance_id,
 		release_group.id as album_id,
-		MIN(release_event.date_year) as release_year,
-		MIN(release_event.date_month) as release_month,
-		MIN(release_event.date_day) as release_day
+		release_event.date_year as release_year,
+		release_event.date_month as release_month,
+		release_event.date_day as release_day
 	FROM recording
+	JOIN artist_credit ON artist_credit.id = recording.artist_credit
 	JOIN track on track.recording = recording.id
-	JOIN medium on medium.id = track.medium -- may cause duplication on medium. There were a comment like this, but groupby should handle.
+	JOIN medium on medium.id = track.medium
 	JOIN release ON release.id = medium.release
 	JOIN release_group on release_group.id = release.release_group
-	JOIN release_event ON release_event.release = release.id
-	JOIN l_release_group_release_group ON l_release_group_release_group.entity0 = release_group.id
-	JOIN medium_format ON medium_format.id = medium.format
-	GROUP BY recording.id, release_group.id
-)
-
-SELECT * FROM recording_to_album;
+	JOIN release_event ON release_event.release = release.id;
+	-- LIMIT 100000;
+	-- JOIN l_release_group_release_group ON l_release_group_release_group.entity0 = release_group.id
+	-- JOIN medium_format ON medium_format.id = medium.format
+	-- GROUP BY recording.id, release_group.id+
+--)
+-- SELECT * from recording_to_album LIMIT 100;
+-- SELECT * FROM recording_to_album;
+-- SELECT * from track
+-- JOIN medium ON medium.id = track.medium
+-- JOIN release ON release.id = medium.release
+-- JOIN release_group on release_group.id = release.release_group
+-- -- GROUP BY recording
+-- LIMIT 10;
+-- SELECT * FROM release_group LIMIT 1;
+-- SELECT * from medium LIMIT 1;
---
+++ scripts/create_jazz_table.py
@@ -0,0 +1,136 @@
+from functools import lru_cache
+import io
+from matplotlib.pylab import norm, normal
+from tempfile import NamedTemporaryFile
+import psycopg
+import pandas as pd
+from jazz_graph.extract_discogs import InMemDiscogs, is_jazz_album, MatchDiscogs
+
+
+class ProcessRows:
+    def __init__(self):
+        self.seen = set()
+        self.data = []
+
+    def process_matched(self, row, matching):
+        # This processes the data so that we get one row per jazz recording.
+        # Note the the most important data is the recording id and discog id.
+        # The discog id will allow us to determine the most appropriate master
+        # album to associate with the recording. BUT we're not yet concerned
+        # with albums. The main data model is performance, musician, song.
+
+        # row = (song, album, artist, recording_id, album_id, release_year, release_month, release_day)
+        discogs_album_id = matching['id']
+        release_year = matching['released']
+        recording_id = row[3]
+        if recording_id in self.seen:
+            return # this recording already exists in the data, so ignore it.
+        data = row + (discogs_album_id, release_year)
+        self.add_data(data)
+
+    def add_data(self, data):
+        self.data.append(data)
+
+    def data_csv_stream(self) -> io.StringIO:
+        df = pd.DataFrame.from_records(
+            self.data,
+            columns=['song', 'album', 'artist', 'recording_id', 'album_id', 'release_year', 'release_month', 'release_day', 'discog_album_id', 'discog_date']
+        )[['recording_id', 'album_id', 'song', 'album', 'artist', 'discog_album_id']]
+        csv_buff = io.StringIO()
+        df.to_csv(csv_buff, index=False)
+        csv_buff.seek(0)
+        return csv_buff
+
+class LoadData():
+    """Load the data."""
+    def __init__(self, connection):
+        self.connection = connection
+
+    def load_data(self, csv_buff):
+        try:
+            # to a table in the database.
+            cursor = self.connection.cursor()
+            self._create_jazz_recordings_table(cursor)
+            # song, album, artist, recording_id, album_id, release_year, release_month, release_day, discog_id, discog_date
+            with cursor.copy("""
+                COPY jazz_recordings (
+                    recording,
+                    record_group,
+                    song_title,
+                    album_title,
+                    artist_name,
+                    discogs_release
+                )
+                FROM STDIN
+                WITH (FORMAT CSV, HEADER)
+            """) as copy:
+                copy.write(csv_buff.read())
+            cursor.close()
+            self.connection.commit()
+        except Exception:
+            self.connection.rollback()
+
+    def _create_jazz_recordings_table(self, cursor):
+        import textwrap
+        cursor.execute("DROP TABLE IF EXISTS jazz_recordings;")
+        query = textwrap.dedent("""
+        CREATE TABLE jazz_recordings (
+            id SERIAL PRIMARY KEY,
+            recording INT NOT NULL,
+            record_group INT,
+            song_title TEXT,
+            album_title TEXT,
+            artist_name TEXT,
+            discogs_release INT,
+            CONSTRAINT fk_recording
+                FOREIGN KEY (recording)
+                REFERENCES recording(id)
+                ON DELETE RESTRICT
+        );
+        """)
+        cursor.execute(query)
+
+class Pipeline:
+    def __init__(self, path_to_query):
+        self.query = self.import_query(path_to_query)
+        self.discogs = MatchDiscogs(InMemDiscogs('/workspace/local_data/jazz_releases.jsonl', is_jazz_album))
+        self.process_rows = ProcessRows()
+
+    def execute(self):
+        with psycopg.connect("dbname=musicbrainz_db user=philosofool") as conn:
+            conn.autocommit = False  # REQUIRED
+
+            cur = conn.cursor(name="my_streaming_cursor")  # named cursor
+            cur.itersize = 10_000
+
+            cur.execute(self.query)
+            n_row_processed = 0
+            n_row_inspected = 0
+            for row in cur:
+                n_row_inspected += 1
+                matched = self.discogs.matching_discog(row)
+                if not matched:
+                    continue
+                self.process_rows.process_matched(row, matched)
+                n_row_processed += 1
+
+            print(f"Finished processing query.\nRows seen: {n_row_inspected}.\nProcessed {n_row_processed} jazz entries with {len(self.process_rows.data)} unique recordings.")
+
+            load_data = LoadData(conn)
+            load_data.load_data(self.process_rows.data_csv_stream())
+            cur.execute("SELECT * FROM jazz_recordings LIMIT 3")
+            for row in cur:
+                print(row)
+            cur.close()
+
+    @staticmethod
+    def import_query(path):
+        with open(path) as f:
+            out = f.read()
+        return out
+
+if __name__ == '__main__':
+    import os
+    sql = '/workspace/queries/recording_to_album.sql'
+    pipeline = Pipeline(sql)
+    pipeline.execute()
