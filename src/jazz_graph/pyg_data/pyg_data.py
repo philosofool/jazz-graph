a6740bf3775de9074f63a74e80b3a307fdc51d8f
philosofool <lenhart.lenhart@gmail.com>
2026-02-14 22:15:41 +0000

    Add labeling and mask methods (WIP) in CreateTensors

--- src/jazz_graph/pyg_data/pyg_data.py
+++ src/jazz_graph/pyg_data/pyg_data.py
@@ -32,36 +32,77 @@ class CreateTensors:
     def artists(self) -> torch.Tensor:
         if self._artists is None:
             self._artists = self.load_parquet('artist_nodes.parquet')
-            # self._artists_lookup = NodeLookUp(self._artists)
 
         return torch_values(self._artists)
 
     def songs(self) -> torch.Tensor:
         if self._songs is None:
             self._songs = self.load_parquet('song_nodes.parquet')
-            # self._songs_lookup = NodeLookUp(self._songs)
         return torch_values(self._songs)
+
+    def labels(self) -> torch.Tensor:
+        if getattr(self, '_labels', None) is None:
+            self._labels = self.load_parquet('performance_nodes.parquet')
+            # NOTE: This is pretty fragile and not currently under a robust test.
+            # We should prbably get the expceted label schema under control somewhere.
+            cols = [column for column in self._labels if column not in  {'release_date', 'recording_id'}]
+            self._labels = self._labels[cols].copy()
+        return torch_values(self._labels)
+
+    def _mask_slices(self, seed=42) -> tuple:
+        # NOTE: this is not probably best for the final version.
+        # I'm simiplifying for the prototype.
+        # we should probably mask whole albums. The large majority
+        # of performances on an album have identical personal and
+        # differ only in song BUT style info is also usually
+        # done at the album level. The task of style classification
+        # or edge prediction is probably too easy if that's all it
+        # takes--you would rarely need more than the immediate
+        # neighborhood of a node to do the prediction. So:
+        # FIXME: these masks should be done at the album grouping level.
+        size = self.performances().size(0)
+        if size > 15_000:
+            raise NotImplementedError("This data is not production ready and should only be used with prototyping date right now.")
+        rng = np.random.default_rng(seed)
+        train_idx = rng.random(size) < .8
+        test_idx = ~train_idx & (rng.random(size) < .5)
+        dev_idx = ~train_idx & ~test_idx
+        return train_idx, dev_idx, test_idx
+
+    def train_mask(self) -> torch.Tensor:
+        return self._mask_slices()[0]
+
+    def dev_mask(self) -> torch.Tensor:
+        return self._mask_slices()[1]
+
+    def test_mask(self) -> torch.Tensor:
+        return self._mask_slices()[2]
+
 
     def performances(self) -> torch.Tensor:
         if self._performances is None:
             self._performances = self.load_parquet('performance_nodes.parquet')
-            # self._performance_lookup = NodeLookUp(self._performances)
+            self._performances['release_date'] = self._performances.release_date.astype('datetime64[ms]').dt.year
+            self._performances = self._performances[['release_date']].copy()
         return torch_values(self._performances)
 
-    def song_artist_edges(self) -> torch.Tensor:
+    def artist_song_edges(self) -> torch.Tensor:
         if getattr(self, '_song_artist_edges', None) is None:
-            self._song_artist_edges = self.load_parquet('song_artist_edges.parquet')
+            df = self.load_parquet('song_artist_edges.parquet')
+            self._artist_song_edges = df[['artist_id', 'work_id']].copy()
 
-        return torch_values(self._song_artist_edges).T
+        return torch_values(self._artist_song_edges).T
 
-    def performance_artist_edges(self) -> torch.Tensor:
+    def artist_performance_edges(self) -> torch.Tensor:
         if getattr(self, '_performance_artist_edges', None) is None:
-            self._performance_artist_edges = self.load_parquet('performance_artist_edges.parquet')
-        return torch_values(self._performance_artist_edges).T
+            df = self.load_parquet('performance_artist_edges.parquet')
+            self._artist_performance_edges = df[['artist_id', 'recording_id']].copy()
+        return torch_values(self._artist_performance_edges).T
 
     def performance_song_edges(self) -> torch.Tensor:
         if getattr(self, '_performance_song_edges', None) is None:
-            self._performance_song_edges = self.load_parquet('performance_song_edges.parquet')
+            df = self.load_parquet('performance_song_edges.parquet')
+            self._performance_song_edges = df[['recording_id', 'work_id']].copy()
         return torch_values(self._performance_song_edges).T
 
 
@@ -79,26 +120,3 @@ def torch_index(df: pd.DataFrame) -> torch.Tensor:
         assert len(data.shape) == 1
         data = data.reshape(-1, 1)
     return torch.tensor(data)
-
-
-if __name__ == '__main__':
-
-    create = CreateTensors('/workspace/local_data/')
-    create.songs()
-    create.artists()
-    create.song_artist_edges().shape
-
-    torch.min(create.performance_artist_edges())
-    torch.max(create.performance_artist_edges())
-
-
-    data = HeteroData()
-    data['performance'].x = ...
-    data['song'].x = ...
-    data['artist'].x = ...
-
-    data['artist', 'performs', 'performance'].edge_index = ...
-    data['performance', 'performing', 'song'].edge_index = ...
-    data['song', 'composes', 'artist'].edge_index = ...
-
-    data['artist', 'performs', 'performance'].edge_attr = ...
--- tests/pyg_data/test_pyg_data.py
+++ tests/pyg_data/test_pyg_data.py
@@ -32,16 +32,17 @@ def _write_parquet(df, filename: str, directory: str = '/workspace'):
     df.to_parquet(path, index=True)
     print(f"Wrote {df.shape[0]} rows, {df.shape[1]} columns to {path}.")
 
-class TestCreateData:
+class TestCreateTensors:
     path = '/workspace/tests/test_pyg_data'
 
     songs = pd.DataFrame({'title': [1, 3, 2]})
     artists = pd.DataFrame({'name': [100, 40, 50]})
-    performances = pd.DataFrame({'free_jazz': [0, 0, 1], 'bop': [1, 0, 1], 'vocal': [0, 1, 1]})
+    performances = pd.DataFrame({'recording_id': [100, 101, 102], 'release_date': pd.to_datetime(['2000', '1956', '1976']), 'free_jazz': [0, 0, 1], 'bop': [1, 0, 1], 'vocal': [0, 1, 1]})
+    performance = performances.astype({'release_date': 'object'})  # assure that if source data is object type, casting will work.
 
-    song_artist_edges = pd.DataFrame({'song': [0, 1, 2], 'artist': [1, 2, 2]})
-    performance_artist_edges = pd.DataFrame({'performance': [0, 0, 1, 1, 2], 'artist': [1, 0, 1, 2, 2]})
-    performance_song_edge = pd.DataFrame({'song': [0, 1, 2], 'performance': [2, 1, 0]})
+    song_artist_edges = pd.DataFrame({'work_id': [0, 1, 2], 'artist_id': [1, 2, 2]})
+    performance_artist_edges = pd.DataFrame({'recording_id': [0, 0, 1, 1, 2], 'artist_id': [1, 0, 1, 2, 2]})
+    performance_song_edge = pd.DataFrame({'work_id': [0, 1, 2], 'recording_id': [2, 1, 0]})
     import os
     if os.path.exists(path):
         _write_parquet(performances, 'performance_nodes.parquet', path)
@@ -54,7 +55,9 @@ class TestCreateData:
 
     def test_performances(self):
         create = CreateTensors(self.path)
-        assert create.performances().shape == (3, 3)
+        assert create.performances().shape == (3, 1)
+        assert create.performances().dtype == torch.int32
+        np.testing.assert_array_equal(create.performances(), np.array([2000, 1956, 1976]).reshape(-1, 1))
 
     def test_songs(self):
         create = CreateTensors(self.path)
@@ -69,17 +72,43 @@ class TestCreateData:
     def test_performance_artist_edges(self):
         create = CreateTensors(self.path)
         expected = torch.tensor([
-            [0, 0, 1, 1, 2],
-            [1, 0, 1, 2, 2]
+            [1, 0, 1, 2, 2],
+            [0, 0, 1, 1, 2]
         ])
-        np.testing.assert_array_equal(create.performance_artist_edges(), expected)
+        np.testing.assert_array_equal(create.artist_performance_edges(), expected)
 
     def test_performance_song_edges(self):
         create = CreateTensors(self.path)
-        expected = np.array([[0, 1, 2], [2, 1, 0]])
+        expected = np.array([
+            [2, 1, 0],
+            [0, 1, 2]
+        ])
         np.testing.assert_array_equal(create.performance_song_edges(), expected)
 
     def test_song_artist_edges(self):
         create = CreateTensors(self.path)
-        expected = torch.tensor([[0, 1, 2], [1, 2, 2]])
-        np.testing.assert_array_equal(create.song_artist_edges(), expected)
+        expected = torch.tensor([
+            [1, 2, 2],
+            [0, 1, 2]
+        ])
+        np.testing.assert_array_equal(create.artist_song_edges(), expected)
+
+    def test__mask_slices(self):
+        create = CreateTensors(self.path)
+        create.performances()
+        # hacky dependence on implementation but we don't need that much rigor here.
+        create._performances = pd.DataFrame(np.arange(0, 40_000).reshape(-1, 4))
+        train, dev, test = create._mask_slices()
+        assert not np.any(train & dev & test), "The slices should be disjoint."
+        assert np.all(train | dev | test), "The slices should be exhaustive."
+
+        # basic size checks, should be approx. ratio 8:1:1.
+        assert np.sum(train) > 7500
+        assert np.sum(test) < 1200
+        assert np.sum(dev) < 1200
+
+    def test_labels(self):
+        create = CreateTensors(self.path)
+        result = create.labels()
+        expected = np.array([[0, 0, 1], [1, 0, 1], [0, 1, 1]]).T
+        np.testing.assert_array_equal(result, expected)
