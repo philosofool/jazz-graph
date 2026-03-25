191446195d6da28605f70c53c2ff4976d892e975
philosofool <lenhart.lenhart@gmail.com>
2026-03-25 23:45:44 +0000

    Rebuild jazz_recordings view.

--- scripts/create_jazz_table.py
+++ scripts/create_jazz_table.py
@@ -241,4 +241,12 @@ if __name__ == '__main__':
             "INSERT INTO schema_migrations (version) VALUES (%s)",
             (current_migration_name(),)
         )
+        create_jazz_recordings = """
+        CREATE OR REPLACE VIEW jazz_recordings AS
+        SELECT discogs_release.id as discogs_id, recording_first_release.*
+        FROM discogs_release
+        JOIN discogs_release_to_recording AS dr2r ON discogs_release.id = dr2r.discogs_id
+        JOIN recording_first_release ON recording_first_release.recording_id = dr2r.recording_id;
+        """
+        cursor.execute(create_jazz_recordings)
         conn.commit()
